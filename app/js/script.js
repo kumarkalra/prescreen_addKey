c68abd560ac2a6598c658638c74af074d82a52d9
Kumar Kalra <kumkal22@gmail.com>
2016-07-10 17:12:40 -0400

    First commit

---
+++ app/js/script.js
@@ -0,0 +1,60 @@
+/**
+ * Called when calculate button is clicked.
+ * @constructor
+ */
+function parseTextArea() {
+  var userInput = document.querySelector(".prescreen-input").value.toLowerCase();
+  userInput = userInput.replace(/ /g, '');
+  var lines = userInput.split('\n');
+  var obj = {};
+
+  for (var i = 0; i < lines.length; ++i) {
+    // split each string on the comma
+    var keyValues = lines[i].split(',');
+    keyValues = keyValues.toString();
+    keyValues = keyValues.split(':');
+
+    // if the current key already exists in the object, add new value to its current value; else, add the new key and its associated value
+    if (obj.hasOwnProperty(keyValues[0])) {
+      // get current key's value
+      var value = obj[keyValues[0]];
+
+      // add new value to existing sum for this key
+      value += parseInt(keyValues[1]);
+
+      // set this key's value to the newly calculated sum
+      obj[keyValues[0]] = value;
+    } else {
+      // add the new key and its associated value to the object
+      obj[keyValues[0]] = parseInt(keyValues[1]);
+    }
+  }
+
+  var output = "";
+
+  for (key in obj) {
+    if (isPalindrome(key)) {
+      output += '<p class="prescreen-palindrome">The total for <span>' + key + '</span> is <span>' + obj[key] + '</span>.</p>';
+    } else {
+      output += '<p class="prescreen-output">The total for <span>' + key + '</span> is <span>' + obj[key] + '</span>.</p>';
+    }
+  }
+  var textnode = document.createTextNode(output);
+  document.querySelector(".output").innerHTML = output;
+
+}
+
+/**
+ * Returns whether passed parameter is a palindrome or no.
+ * @constructor
+ * @param {string} strValue - string value passed for test of palindrome.
+ */
+function isPalindrome(strValue) {
+  var str = strValue.replace(/[^A-Z]+/ig, "");
+  var len = Math.floor(str.length / 2);
+  for (var i = 0; i < len; i++) {
+    if (str[i] !== str[str.length - i - 1])
+      return false;
+  }
+  return true;
+}
---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+// Gulp Dependencies
+var gulp = require('gulp');
+
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+// Sync with the browser
+var browserSync = require('browser-sync').create();
+
+// concatinate the js files
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+
+// Cleaning up generated files automatically
+var del = require('del');
+
+// JSDoc
+var jsdoc = require('gulp-jsdoc3');
+
+// To run tasks in sequence
+var runSequence = require('run-sequence');
+
+gulp.task('hello', function() {
+  console.log('Hello Zell');
+});
+
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Gulp watch syntax
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+// Changes Sync with browser
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+gulp.task('cache:clear', function (callback) {
+return cache.clearAll(callback)
+});
+
+gulp.task('build', ['clean', 'sass', 'useref'], function (){
+  console.log('Building files');
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref'],
+    callback
+  )
+});
+
+gulp.task('default', function (callback) {
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
+});
+
+gulp.task('doc', function (cb) {
+    gulp.src(['README.md', './app/js/**/*.js'], {read: false})
+        .pipe(jsdoc(cb));
+});
